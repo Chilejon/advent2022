7d2ca3edf52a97d94b38ed7858ff02828b2219ae
Jon Chiles <jonchiles@yahoo.com>
2022-12-19 18:29:44 +0000

    day 4

--- Advent2022/Program.cs
+++ Advent2022/Program.cs
@@ -1,12 +1,124 @@
 ﻿using Microsoft.Win32;
 using System.Collections;
+using System.Diagnostics.Metrics;
+using System.Collections.Immutable;
 
 //elves();
 
 //rps();
 
-packing();
+//packing();
 
+cleaning();
+
+void cleaning()
+{
+	string cleaningData = File.ReadAllText(@"C:\code\Advent2022\Advent2022\Data\day4.txt");
+
+	string[] cleans = cleaningData.Split("\r\n");
+
+	int score = 0;
+	int overlapped = 0;
+
+	foreach (string clean in cleans)
+	{
+		string[] cleansplit = clean.Split(",");
+		string[] leftElf = cleansplit[0].Split("-");
+		string[] rightElf = cleansplit[1].Split("-");
+
+		var leftElfdiff = int.Parse(leftElf[1]) - int.Parse(leftElf[0]);
+		var rightElfdiff = int.Parse(rightElf[1]) - int.Parse(rightElf[0]);
+
+		var leftElfArea = string.Empty;
+		var rightElfArea = string.Empty;
+
+
+		for (int i = 0; i < leftElfdiff; i++)
+		{
+			leftElfArea = leftElfArea + (int.Parse(leftElf[0]) + i) + "-";
+		}
+
+		for (int i = 0; i < rightElfdiff; i++)
+		{
+			rightElfArea = rightElfArea + (int.Parse(rightElf[0]) + i) + "-";
+		}
+
+		if (leftElfArea.Length > 1)
+		{
+			leftElfArea = leftElfArea.Remove(leftElfArea.Length - 1, 1);
+		}
+		if (rightElfArea.Length > 1)
+		{
+			rightElfArea = rightElfArea.Remove(rightElfArea.Length - 1, 1);
+		}
+
+		if (rightElfArea.Contains(leftElfArea))
+		{
+			overlapped++;
+			Console.WriteLine("L in R overlap: " + leftElfArea + " " + rightElfArea);
+		}
+
+		if (leftElfArea.Contains(rightElfArea))
+		{
+			overlapped++;
+			Console.WriteLine("R in L overlap: " + leftElfArea + " " + rightElfArea);
+		}
+		
+
+
+
+		//leftElfArea = leftElf[0].ToString();
+		//if (leftElf[0] != leftElf[0])
+		//{
+		//	leftElfArea += int.Parse(leftElf[0]) + 1;
+		//}
+		//rightElfArea = rightElf[0].ToString();
+
+	}
+
+
+	Console.WriteLine("overlap: " + overlapped);
+
+	var assignedSections = File.ReadLines(@"C:\code\Advent2022\Advent2022\Data\day4.txt").ToList();
+	var overlappingSectionsCount = 0;
+	assignedSections.ForEach(pair =>
+	{
+		var sectionRanges = ParseSections(pair).ToImmutableArray();
+		if (IsOverlapping(sectionRanges[0], sectionRanges[1])) overlappingSectionsCount++;
+	});
+	Console.WriteLine($"There are {overlappingSectionsCount} assigned sections that are overlapping.");
+
+	var intersectingSectionsCount = 0;
+	assignedSections.ForEach(pair =>
+	{
+		var sectionRanges = ParseSections(pair).ToImmutableArray();
+		if (IsIntersecting(sectionRanges[0], sectionRanges[1])) intersectingSectionsCount++;
+	});
+	Console.WriteLine($"There are {intersectingSectionsCount} assigned sections that are partially overlapping.");
+
+
+}
+
+IEnumerable<Range> ParseSections(string assignmentPair)
+{
+	return assignmentPair.Split(',').Select(GetRange).ToList();
+}
+
+Range GetRange(string assignmentPair)
+{
+	var sections = assignmentPair.Split('-').Select(int.Parse).ToArray();
+	return new Range(sections[0], sections[1]);
+}
+
+bool IsOverlapping(Range startRange, Range endRange)
+{
+	return (startRange.Start.Value <= endRange.Start.Value && startRange.End.Value >= endRange.End.Value) || (endRange.Start.Value <= startRange.Start.Value && endRange.End.Value >= startRange.End.Value);
+}
+
+bool IsIntersecting(Range startRange, Range endRange)
+{
+	return startRange.Start.Value <= endRange.End.Value && endRange.Start.Value <= startRange.End.Value;
+}
 
 void packing()
 {
